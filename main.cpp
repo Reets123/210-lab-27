aa89302a6bebc5c60dd5ee82cc7ff108b040806a
Reets123 <ibrahimalatig@gmail.com>
2024-10-30 09:01:59 +0000

    commit 1

--- main.cpp
+++ main.cpp
@@ -8,4 +8,14 @@
 
 using namespace std;
 
-void displayVillage
+void displayVillagers(const map<string, tuple<int, string, string>>& villagers) {
+    cout << "Villager details:\n";
+    for (const auto& [name, details] : villagers) {
+        cout << name << " ["
+             << get<0>(details) << ", "
+             << get<1>(details) << ", "
+             << get<2>(details) << "]\n";
+    }
+}
+
+int m
